709c23a48afadd140bf93b173e01c6521c4bdd6c
brais <uo296092@uniovi.es>
2025-12-02 18:41:08 +0100

    Finalizada la practica 7.1

--- app/src/main/java/es/uniovi/converter/MainActivity.kt
+++ app/src/main/java/es/uniovi/converter/MainActivity.kt
@@ -1,13 +1,17 @@
 package es.uniovi.converter
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import org.json.JSONObject
 import retrofit2.Response
 import retrofit2.Retrofit
@@ -20,9 +24,11 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-    var euroToDollar: Double = 1.16
+    //var euroToDollar: Double = 1.16
     lateinit var editTextEuros: EditText
     lateinit var editTextDollars: EditText
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -32,18 +38,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        viewModel.fetchExchangeRate()
         editTextEuros = findViewById(R.id.editTextEuros)
         editTextDollars = findViewById(R.id.editTextDollars)
     }
 
     fun onClickToDollars(view: View) {
-        Toast.makeText(this, "¡Me han pulsado!", Toast.LENGTH_SHORT).show()
-        convert(editTextEuros,editTextDollars,euroToDollar)
+        val factor = viewModel.euroToDollar
+        convert(editTextEuros,editTextDollars,factor)
     }
 
     fun onClickToEuros (view: View) {
-        Toast.makeText(this, "¡Me han pulsado!", Toast.LENGTH_SHORT).show()
-        convert(editTextDollars,editTextEuros,euroToDollar)
+        val factor = viewModel.euroToDollar
+        convert(editTextDollars,editTextEuros,factor)
     }
 
     private fun convert(source: EditText, destination: EditText, factor: Double) {
@@ -56,52 +63,6 @@ class MainActivity : AppCompatActivity() {
         destination.setText((value * factor).toString())
     }
 
-    private fun fetchExchangeRate() {
-        val url = URL("https://api.frankfurter.app/latest?from=EUR&to=USD")
-        val connection = url.openConnection() as HttpURLConnection
-        connection.requestMethod = "GET"
-        val inputStream = connection.inputStream
-        val reader = BufferedReader(InputStreamReader(inputStream))
-        val response = StringBuilder()
-        var line: String?
-        while (reader.readLine().also { line = it } != null) {
-            response.append(line)
-        }
-        reader.close()
-        val jsonResponse = response.toString()
-        val jsonObject = JSONObject(jsonResponse)
-        val rates = jsonObject.getJSONObject("rates")
-        euroToDollar = rates.getDouble("USD")
-    }
 }
-data class Rates(
-    val USD: Double
-)
-data class ExchangeRateResponse(
-    val amount: Double,
-    val base: String,
-    val date: String,
-    val rates: Rates
-)
-interface ExchangeRateApi {
-    @GET("latest")
-    suspend fun convert(
-        @Query("from") from: String,
-        @Query("to") to: String,
-        @Query("amount") amount: Double
-    ): Response<ExchangeRateResponse>
-}
-object RetrofitClient {
-    private const val BASE_URL = "https://api.frankfurter.app/"
 
-    private val retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
 
-    val api: ExchangeRateApi by lazy {
-        retrofit.create(ExchangeRateApi::class.java)
-    }
-}
